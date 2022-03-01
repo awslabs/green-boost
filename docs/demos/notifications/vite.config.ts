822aaf4a720dcdf218ee2267fd64fb911ec1b4be
Ben Stickley <bestickley@gmail.com>
2022-03-01 15:55:18 -0500

    docs: use static assets for notifications demo

--- docs/demos/notifications/vite.config.ts
+++ docs/demos/notifications/vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        // https://ui.docs.amplify.aws/getting-started/installation#vite
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+    ],
+  },
 });
