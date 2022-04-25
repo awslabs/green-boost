9be3990883c052b41b2a8a33451f57d3dadc3057
Ben Stickley <bestickley@gmail.com>
2022-04-25 11:41:53 -0400

    refactor: suppressAwsManagedConstructs -> SuppressOkNags

--- packages/gboost-infra/src/SuppressOkNags.ts
+++ packages/gboost-infra/src/SuppressOkNags.ts
@@ -1,27 +1,16 @@
-import { Stack } from "aws-cdk-lib";
+import { IAspect } from "aws-cdk-lib";
 import { NagSuppressions } from "cdk-nag";
+import { IConstruct } from "constructs";
 
 /**
  * Suppresses cdk-nag errors for AWS managed constructs regarding log
  * retention, bucket deployments, and bucket notifications
  */
-export function suppressAwsManagedConstructs(stack: Stack) {
-  for (const construct of stack.node.findAll()) {
-    if (construct.node.path.includes("LogRetention")) {
-      NagSuppressions.addResourceSuppressions(construct, [
-        {
-          id: "AwsSolutions-IAM4",
-          reason:
-            "The Lambda Basic Execution Role does not grant excessive access",
-        },
-        {
-          id: "AwsSolutions-IAM5",
-          reason:
-            "The CDK creates this role so that it can set custom retention periods on any CloudWatch Logs resource that gets created.",
-        },
-      ]);
-    } else if (construct.node.path.includes("Custom::CDKBucketDeployment")) {
-      NagSuppressions.addResourceSuppressions(construct, [
+export class SuppressOkNags implements IAspect {
+  visit(node: IConstruct) {
+    const path = node.node.path;
+    if (path.includes("Custom::CDKBucketDeployment")) {
+      NagSuppressions.addResourceSuppressions(node, [
         {
           id: "AwsSolutions-IAM4",
           reason:
@@ -33,8 +22,8 @@ export function suppressAwsManagedConstructs(stack: Stack) {
             "Wildcards are used to succinctly define a specific set or permissions to defined resources.",
         },
       ]);
-    } else if (construct.node.path.includes("BucketNotificationsHandler")) {
-      NagSuppressions.addResourceSuppressions(construct, [
+    } else if (path.includes("BucketNotificationsHandler")) {
+      NagSuppressions.addResourceSuppressions(node, [
         {
           id: "AwsSolutions-IAM4",
           reason:
