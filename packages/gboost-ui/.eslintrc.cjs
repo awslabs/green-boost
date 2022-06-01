18dd05112afee29bac36c6c0f4044140a39123fe
Ben Stickley <bestickley@gmail.com>
2022-06-01 12:05:59 -0400

    fix: lint and audit github action workflows

--- packages/gboost-ui/.eslintrc.cjs
+++ packages/gboost-ui/.eslintrc.cjs
@@ -3,11 +3,22 @@
 const { defineConfig } = require("eslint-define-config");
 
 module.exports = defineConfig({
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
   extends: [
     "react-app", // use CRA linting rules
     "react-app/jest",
-    "../../.eslintrc.cjs",
   ],
+  ignorePatterns: ["node_modules", "lib"],
+  parser: "@typescript-eslint/parser", // This allows ESLint to understand TypeScript syntax
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // reduces development bundle size speeding up dev server
@@ -15,3 +26,15 @@ module.exports = defineConfig({
     "no-restricted-imports": ["error", "react-icons"],
   },
 });
+
+/**
+ * Cannot use root .eslintrc.cjs b/c of this error:
+    packages/gboost-ui lint: Oops! Something went wrong! :(
+    packages/gboost-ui lint: ESLint: 8.13.0
+    packages/gboost-ui lint: ESLint couldn't determine the plugin "@typescript-eslint" uniquely.
+    packages/gboost-ui lint: - /Users/stickb/Code/green-boost/node_modules/.pnpm/@typescript-eslint+eslint-plugin@5.27.0_xgwjwvswzzo77lpghh6plzerx4/node_modules/@typescript-eslint/eslint-plugin/dist/index.js (loaded in ".eslintrc.cjs » ../../.eslintrc.cjs » plugin:@typescript-eslint/recommended » ./configs/base")
+    packages/gboost-ui lint: - /Users/stickb/Code/green-boost/node_modules/.pnpm/@typescript-eslint+eslint-plugin@5.27.0_hc5nseoddt63bvpdva6z5vpn44/node_modules/@typescript-eslint/eslint-plugin/dist/index.js (loaded in ".eslintrc.cjs » eslint-config-react-app#overrides[0]")
+    packages/gboost-ui lint: Please remove the "plugins" setting from either config or remove either plugin installation.
+    packages/gboost-ui lint: If you still can't figure out the problem, please stop by https://eslint.org/chat/help to chat with the team.
+    packages/gboost-ui lint: Failed
+ */
