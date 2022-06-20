fda5bb25dbf5084f71b747720697c4eed281d1c7
Ben Stickley <bestickley@gmail.com>
2022-06-20 15:25:52 -0400

    fix(ui): StyledButton now shows correct color for disabled

--- packages/gboost-ui/src/components/StyledButton.tsx
+++ packages/gboost-ui/src/components/StyledButton.tsx
@@ -2,66 +2,55 @@ import { Button } from "@aws-amplify/ui-react";
 import { styled } from "../stitches.config.js";
 
 export const StyledButton = styled(Button, {
+  "&:disabled": {
+    bc: "$gray9",
+  },
   variants: {
     color: {
       success: {
         bc: "$success9",
-        color: "white !important",
-        "&:hover": {
-          bc: "$success10 !important",
-        },
-        "&:focus": {
-          bc: "$success10 !important",
+        color: "white",
+        "&:hover, &:focus": {
+          bc: "$success10",
+          color: "white",
         },
       },
       error: {
         bc: "$error9",
-        color: "white !important",
-        "&:hover": {
-          bc: "$error10 !important",
-        },
-        "&:focus": {
-          bc: "$error10 !important",
+        color: "white",
+        "&:hover:enabled, &:focus:enabled": {
+          bc: "$error10",
+          color: "white",
         },
       },
       info: {
         bc: "$info9",
-        color: "white !important",
-        "&:hover": {
-          bc: "$info10 !important",
-        },
-        "&:focus": {
-          bc: "$info10 !important",
+        color: "white",
+        "&:hover:enabled, &:focus:enabled": {
+          bc: "$info10",
+          color: "white",
         },
       },
       warn: {
         bc: "$warn9",
-        color: "$warn12 !important",
-        "&:hover": {
-          bc: "$warn10 !important",
-        },
-        "&:focus": {
-          bc: "$warn10 !important",
+        color: "$warn12",
+        "&:hover:enabled, &:focus:enabled": {
+          bc: "$warn10",
+          color: "$warn12",
         },
       },
       primary: {
         bc: "$primary9",
-        color: "$primary12 !important",
-        "&:hover": {
-          bc: "$primary10 !important",
-        },
-        "&:focus": {
-          bc: "$primary10 !important",
+        color: "$primary12",
+        "&:hover:enabled, &:focus:enabled": {
+          bc: "$primary10",
         },
       },
       secondary: {
         bc: "$secondary9",
-        color: "$secondary12 !important",
-        "&:hover": {
-          bc: "$secondary10 !important",
-        },
-        "&:focus": {
-          bc: "$secondary10 !important",
+        color: "$secondary12",
+        "&:hover:enabled, &:focus:enabled": {
+          bc: "$secondary10",
         },
       },
     },
