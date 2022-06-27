7410a9b4e5639aad1f3421f4c61a60bb379caa5c
Ben Stickley <bestickley@gmail.com>
2022-06-27 14:04:39 -0400

    feat(infra): add GovCloudCompat aspect

--- packages/gboost-infra/src/index.ts
+++ packages/gboost-infra/src/index.ts
@@ -3,7 +3,7 @@ export * from "./common-props.js";
 export * from "./function.js";
 export * from "./graphql-api.js";
 export * from "./static-site/index.js";
-export * from "./SuppressOkNags.js";
+export * from "./aspects/index.js";
 export * from "./table.js";
 export * from "./user-management/index.js";
 export * from "./user-base/index.js";
