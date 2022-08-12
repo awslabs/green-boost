4b981ff19c7efb6e87e856761cf9850105c098ef
Ben Stickley <bestickley@gmail.com>
2022-08-12 08:39:55 -0400

    feat: Add NoData prop to QueryTable to customize component shown when no data

---
+++ .changeset/grumpy-sloths-bake.md
@@ -0,0 +1,5 @@
+---
+"gboost-ui": minor
+---
+
+Add NoData prop to QueryTable to customize component shown when no data
---
+++ packages/gboost-ui/src/QueryTable/DefaultNoData.tsx
@@ -0,0 +1,18 @@
+import { Alert } from "@aws-amplify/ui-react";
+import { ReactElement } from "react";
+import { styled } from "../index.js";
+
+const StyledAlert = styled(Alert, { mt: "$1" });
+
+interface DefaultNoDataProps {
+  height?: number;
+}
+
+export function DefaultNoData(props: DefaultNoDataProps): ReactElement {
+  const { height } = props;
+  return (
+    <StyledAlert height={height} variation="info">
+      No Data
+    </StyledAlert>
+  );
+}
