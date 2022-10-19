107eacc69b2c17339ceabcdba3536de4c5b8198b
Ben Stickley <bestickley@gmail.com>
2022-10-19 11:18:53 -0400

    fix: TableCell rendering

--- packages/gboost-ui/src/QueryTable/TableCell.tsx
+++ packages/gboost-ui/src/QueryTable/TableCell.tsx
@@ -16,15 +16,11 @@ interface TableCellProps<T> {
 export function TableCell<T>(props: TableCellProps<T>): ReactElement {
   const { col, padding, row } = props;
   let Cell: ReactElement | undefined | string = undefined;
-  let value = "";
-  const id = String(col.id);
-  if (id in row) {
-    value = String(row[col.id as keyof T]);
-  }
+  const value = row[col.id as keyof T];
   if (col.renderCell) {
     Cell = col.renderCell(value, row);
   } else {
-    Cell = value;
+    Cell = value as any;
   }
   return (
     <StyledTableCell css={{ padding }} className="amplify-table__td">
