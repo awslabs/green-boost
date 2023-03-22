cb97ed7ecc9f9bd0aa709bc7d338a3617c4ef1c8
Ben Stickley <bestickley@gmail.com>
2023-03-22 14:51:27 -0400

    test: improve gboost create test

--- packages/gboost/tests/test-create.ts
+++ packages/gboost/tests/test-create.ts
@@ -1,14 +1,14 @@
-import { execSync } from "node:child_process";
+import { exec } from "node:child_process";
 import type { Answers } from "../src/create/ask.js";
 import { Template } from "../src/create/templates.js";
 import { getTemplateOperations } from "../src/create/get-template-operations/get-template-operations.js";
 import { executeOperations } from "../src/create/execute-operations.js";
 import { tmpdir } from "node:os";
-import { mkdtempSync, rmSync } from "node:fs";
+import { mkdtemp, rm } from "node:fs/promises";
 import { resolve as resolvePath } from "node:path";
-import { Option, program } from "@commander-js/extra-typings";
+import { Command, Option } from "@commander-js/extra-typings";
 
-const allTemplates = Object.values(Template);
+// const allTemplates = Object.values(Template);
 
 const templateAnswers: Record<Template, Pick<Answers, "appId" | "appTitle">> = {
   [Template.Minimal]: { appId: "min", appTitle: "Minimal" },
@@ -21,72 +21,80 @@ const templateAnswers: Record<Template, Pick<Answers, "appId" | "appTitle">> = {
   [Template.Dashboard]: { appId: "dash", appTitle: "Dashboard" },
 };
 
+const program = new Command();
 program
+  .name("test-create")
+  .description("CLI to test 'gboost create' templates")
   .option("--all")
   .addOption(
     new Option("--template <template>").choices(Object.values(Template))
   )
   .action(async ({ all, template }) => {
-    if (all) {
-      await testCreate(
-        allTemplates.map((t) => ({ ...templateAnswers[t], template: t }))
-      );
-    } else if (template && template in Template) {
-      await testCreate([
-        { ...templateAnswers[template as Template], template },
-      ]);
-    } else {
-      throw new Error(
-        `Usage: ts-node tests/test-create.ts --template ${allTemplates.join(
-          "|"
-        )}`
-      );
+    try {
+      if (all) {
+        // TODO: uncomment below after all templates are done
+        // await testCreate(
+        //   allTemplates.map((t) => ({ ...templateAnswers[t], template: t }))
+        // );
+        await testTemplates([
+          { ...templateAnswers.Minimal, template: Template.Minimal },
+          { ...templateAnswers.CrudDynamo, template: Template.CrudDynamo },
+        ]);
+      } else if (template && template in Template) {
+        await testTemplates([
+          { ...templateAnswers[template as Template], template },
+        ]);
+      } else {
+        program.help();
+      }
+    } catch (err) {
+      console.error(err);
     }
   });
+program.parse();
 
-function testCreate(answers: Omit<Answers, "directory">[]) {
-  const promises: Promise<unknown>[] = [];
-  for (const answer of answers) {
-    promises.push(
-      new Promise((resolve, reject) => {
-        console.log(`Testing Template ${answer.template}`);
-        const tmpDir = getTmpDir();
-        console.log("Using tmp directory: " + tmpDir);
-        let error: unknown;
+function testTemplates(answers: Omit<Answers, "directory">[]) {
+  // .allSettled() allows working templates to finish before failing
+  return Promise.allSettled(
+    answers.map(async (answer) => {
+      const log = (d: string) => console.log(`${answer.template}: ${d}`);
+      const tmpDir = await mkdtemp(tmpdir());
+      try {
+        log("Testing Template");
+        log(`Using tmp directory: ${tmpDir}`);
+        const operations = getTemplateOperations({
+          ...answer,
+          directory: tmpDir,
+        });
+        executeOperations(operations);
+        await run("pnpm i", tmpDir, log);
+        await run("pnpm typecheck", tmpDir, log);
+        await run("pnpm lint", tmpDir, log);
+        await run("pnpm test", tmpDir, log);
+        const infraDir = resolvePath(tmpDir, "infra");
         try {
-          const operations = getTemplateOperations({
-            ...answer,
-            directory: tmpDir,
-          });
-          executeOperations(operations);
-          run("pnpm i", tmpDir);
-          run("pnpm typecheck", tmpDir);
-          run("pnpm lint", tmpDir);
-          run("pnpm test", tmpDir);
-          run("pnpm deploy:local", resolvePath(tmpDir, "infra"));
-          run("pnpm destroy:local", resolvePath(tmpDir, "infra"));
-          rmSync(tmpDir, { force: true, recursive: true });
+          await run("pnpm deploy:local", infraDir, log);
         } catch (err) {
-          error = err;
+          log("Error while deploying");
+        } finally {
+          await run("pnpm destroy:local", infraDir, log);
         }
-        if (error) {
-          reject(error);
-        } else {
-          resolve(null);
-        }
-      })
-    );
-  }
-  return Promise.all(promises);
+      } catch (err) {
+        //
+      } finally {
+        await rm(tmpDir, { force: true, recursive: true });
+      }
+    })
+  );
 }
 
-function run(command: string, cwd: string) {
-  execSync(command, {
-    cwd,
-    stdio: "inherit",
+function run(command: string, cwd: string, log: (d: string) => void) {
+  return new Promise((resolve, reject) => {
+    const process = exec(command, {
+      cwd,
+    });
+    process.stdout?.on("data", (d) => log(d.toString()));
+    process.stderr?.on("data", (d) => log(d.toString()));
+    process.on("exit", (code) => (code === 0 ? resolve(null) : reject(code)));
   });
-}
-
-function getTmpDir() {
-  return mkdtempSync(tmpdir());
 }
