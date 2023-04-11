e86332fad1a399aaf6d06b3339e7985b9c4519bc
nganoro <nganoro@amazon.com>
2023-04-11 16:59:02 -0400

    initial commit of component ticket

--- examples/widgets-dynamo/ui/vite.config.ts
+++ examples/widgets-dynamo/ui/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-// @ts-ignore
 import { vanillaExtractPlugin as veEsbuildPlugin } from "@vanilla-extract/esbuild-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 import _react from "@vitejs/plugin-react";
