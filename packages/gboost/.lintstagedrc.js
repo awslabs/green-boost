a4a499bfed783c7f3c1affe217b3d23a80ea52c1
Jim Higgins <121066492+jwhigg@users.noreply.github.com>
2023-07-11 19:08:06 -0500

    Add DB IAM Username normalization and vitest fix (#224)

--- packages/gboost/.lintstagedrc.js
+++ packages/gboost/.lintstagedrc.js
@@ -3,6 +3,6 @@ import { removeIgnoredFiles } from "../../utils/lintStaged.js"
 export default {
   "*.ts": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
-    return [`eslint --fix --max-warnings=0 ${filesToLint}`, `vitest related ${files}`, "tsc --noEmit"];
+    return [`eslint --fix --max-warnings=0 ${filesToLint}`, `vitest related ${files} --run`, "tsc --noEmit"];
   }
 }
