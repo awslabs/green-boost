21429cfc2d5f52abb9f63bc31984d5bcaffb02ae
Ben Stickley <bestickley@gmail.com>
2023-08-16 16:20:34 -0400

    fix: update url

--- docs/src/theme.config.tsx
+++ docs/src/theme.config.tsx
@@ -3,6 +3,7 @@ import { type DocsThemeConfig, useConfig } from "nextra-theme-docs";
 const title = "Green Boost";
 const description = "Build Full Stack Cloud Native Web Apps on AWS Fast";
 const repo = "https://github.com/awslabs/green-boost";
+const url = "https://awslabs.github.io/green-boost";
 
 const docsThemeConfig: DocsThemeConfig = {
   docsRepositoryBase: repo + "/tree/main/docs",
@@ -31,10 +32,12 @@ const docsThemeConfig: DocsThemeConfig = {
       additionalMetaTags: [
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
       ],
+      canonical: url,
       description,
       openGraph: {
         title,
         description,
+        url,
       },
       themeColor: "hsl(141.89999999999998, 69.2%, 58%)",
       title: pageTitle ? `${pageTitle} - ${title}` : title,
